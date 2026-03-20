443c8865881ccfe538cc77ddac3557f30cc9a2e4
alexandereynolds0-cell <alexandereynolds0@gmail.com>
2026-03-20 21:57:28 +0800

    Replace binary app icons with XML resources

--- OpenClawPapers/__tests__/App.test.tsx
+++ OpenClawPapers/__tests__/App.test.tsx
@@ -6,6 +6,15 @@ import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
+jest.mock('react-native-webview', () => {
+  const ReactNativeWebViewMock = require('react');
+  const { View } = require('react-native');
+
+  return {
+    WebView: ReactNativeWebViewMock.forwardRef((props, _ref) => ReactNativeWebViewMock.createElement(View, props)),
+  };
+});
+
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
     ReactTestRenderer.create(<App />);
