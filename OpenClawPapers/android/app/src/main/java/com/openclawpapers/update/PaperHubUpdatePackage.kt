29dd2d99ed694e71ff362567e215a171c4880851
alexandereynolds0-cell <alexandereynolds0@gmail.com>
2026-03-21 16:26:16 +0800

    Remove empty ANDROID_HOME from release workflow

---
+++ OpenClawPapers/android/app/src/main/java/com/openclawpapers/update/PaperHubUpdatePackage.kt
@@ -0,0 +1,13 @@
+package com.openclawpapers.update
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class PaperHubUpdatePackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
+    listOf(PaperHubUpdateModule(reactContext))
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> = emptyList()
+}
